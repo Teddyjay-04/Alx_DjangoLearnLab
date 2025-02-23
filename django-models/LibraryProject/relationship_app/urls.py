c9e8559b061a16488c20257b9e05f37b223e319a
Teddyjay-04 <jamesteddy411@gmail.com>
2025-02-23 08:41:53 +0000

    mmmm

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views  # Import views from the current app
 from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
@@ -14,5 +15,6 @@ urlpatterns = [
     path('add-book/', add_book, name='add_book'),
     path('edit-book/<int:book_id>/', edit_book, name='edit_book'),
     path('delete-book/<int:book_id>/', delete_book, name='delete_book'),
+    path('books/', include('relationship_app.urls')),
 
 ]
