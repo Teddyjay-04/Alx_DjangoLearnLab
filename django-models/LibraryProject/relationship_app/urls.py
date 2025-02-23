72843d7349ea243ed03721328c0f11b207a200c2
Teddyjay-04 <jamesteddy411@gmail.com>
2025-02-23 08:24:45 +0000

    42

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('member-view/', views.member_view, name='member_view'),
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view')
+
 ]
