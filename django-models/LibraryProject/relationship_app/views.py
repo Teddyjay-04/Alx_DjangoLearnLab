e167794e62285fc6c87be2c33323737c03ae2536
Teddyjay-04 <jamesteddy411@gmail.com>
2025-02-23 07:26:55 +0000

    modified relationship_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-from .models import Book
-from django.views.generic import DetailView
-from .models import Library
+from django.views.generic.detail import DetailView  # ✅ Explicit import for DetailView
+from .models import Book, Library
 
 def list_books(request):
-    """Function-based view to list all books."""
+    """Function-based view to list all books in the database."""
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, "list_books.html", {"books": books})
 
 class LibraryDetailView(DetailView):
     """Class-based view to display details of a specific library."""
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-    
+    template_name = "library_detail.html"
+    context_object_name = "library"
