d393bad1c2def0cec4d4bb3e4e4e44d1688f8f91
Teddyjay-04 <jamesteddy411@gmail.com>
2025-02-24 22:48:23 +0000

    Updated 2

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -13,6 +13,13 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can edit a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+
     def __str__(self):
         return self.title
 
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,7 @@ from .views import list_books, LibraryDetailView
 from .views import user_login, user_logout, register
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),  # Function-based view
@@ -13,4 +14,7 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
