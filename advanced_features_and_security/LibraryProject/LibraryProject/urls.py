39865d0ff7e63661ae9794cf3b5f3360cb6baa45
Teddyjay-04 <jamesteddy411@gmail.com>
2025-02-27 22:02:16 +0000

    Added book_list view with permission enforcement

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', book_list, name='book_list'),
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render, redirect
 from .models import Book
 from .forms import BookForm
 
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
 @permission_required('bookshelf.can_create', raise_exception=True)
 def add_book(request):
     if request.method == "POST":
