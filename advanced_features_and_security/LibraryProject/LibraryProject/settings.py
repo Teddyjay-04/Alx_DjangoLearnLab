1edddffdb6ba72c26adcd8f8dc481dc31d7e154e
Teddyjay-04 <jamesteddy411@gmail.com>
2025-03-02 12:22:34 +0000

    Added SECURE_PROXY_SSL_HEADER to enforce HTTPS behind proxies

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -150,3 +150,5 @@ X_FRAME_OPTIONS = "DENY"  # Prevents clickjacking attacks
 SECURE_CONTENT_TYPE_NOSNIFF = True  # Prevents MIME-type sniffing
 SECURE_BROWSER_XSS_FILTER = True  # Enables browser’s XSS filtering
 SECURE_SSL_REDIRECT = True  # Redirect all HTTP requests to HTTPS
+# Enable HTTPS handling when behind a proxy (e.g., load balancer)
+SECURE_PROXY_SSL_HEADER =True ('HTTP_X_FORWARDED_PROTO', 'https')
