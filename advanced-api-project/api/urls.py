0ca29d16927710409dd42190f8f14011d9589fd6
Teddyjay-04 <jamesteddy411@gmail.com>
2025-03-16 17:10:06 +0000

    Urls.api

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve a specific book
     path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),  # Update a specific book
     path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),  # Delete a specific book
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+]
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Include the api.urls for API endpoints
 ]
