c318d8e2d36ca33fbeac1951b3a81a97daa220d4
Teddyjay-04 <jamesteddy411@gmail.com>
2025-03-16 17:05:54 +0000

    Configured URLs, permissions, and API views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from django.contrib import admin
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List all books and create a new book
@@ -7,4 +8,7 @@ urlpatterns = [
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve a specific book
     path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),  # Update a specific book
     path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),  # Delete a specific book
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,12 +4,6 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
-
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from .models import Book
-from .serializers import BookSerializer
-
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
