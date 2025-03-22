b2657b24d6561b0ce5e368cd99c486df3331bd57
Teddyjay-04 <jamesteddy411@gmail.com>
2025-03-22 22:12:36 +0000

    Checks for the implementation of the CRUD operations

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .forms import UserRegisterForm, UserUpdateForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+from .forms import PostForm
 
 # Register view
 def register(request):
@@ -28,3 +33,55 @@ def profile(request):
     else:
         form = UserUpdateForm(instance=request.user)
     return render(request, 'blog/profile.html', {'form': form})
+
+
+
+class PostListView(ListView):
+    """Displays a list of all blog posts."""
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    """Displays a single blog post."""
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """Allows authenticated users to create a new blog post."""
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        """Assigns the logged-in user as the author of the post."""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Allows the author of a blog post to update it."""
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        """Ensures the logged-in user remains the author."""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        """Restricts editing to the post's author."""
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Allows the author to delete their blog post."""
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        """Restricts deletion to the post's author."""
+        post = self.get_object()
+        return self.request.user == post.author
