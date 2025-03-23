8b523c058b6e81eefdd2b4c27cf152334d15d155
Teddyjay-04 <jamesteddy411@gmail.com>
2025-03-23 03:20:28 +0000

    Checks for Modify Post Creation and Update Forms task

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Comment
+from .models import Post
+from taggit.forms import TagWidget
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -21,3 +23,11 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),  # Add this to make tag selection easier
+        }
